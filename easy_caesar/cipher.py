7885f280dff49210877b8d31702925d97f4be06b
Sam Blumenthal <sam.sam.42@gmail.com>
2020-08-31 16:28:27 -0400

    Formatting

--- easy_caesar/cipher.py
+++ easy_caesar/cipher.py
@@ -5,6 +5,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
 class Cipher:
     """
     This class encapsulates the cipher object. Providing configuration options and exposing the
@@ -15,26 +16,25 @@ class Cipher:
         self._cipher_length = cipher_length
         self._dbg = debug
 
-
-    def _modify(self, input_str: str, offset: int, encoding : str) -> str:
+    def _modify(self, input_str: str, offset: int, encoding: str) -> str:
         ba = bytearray(input_str, encoding)
-        if (self._dbg):
+        if self._dbg:
             log.info("BA: %s", ba)
         for i, item in enumerate(ba):
-            if (self._dbg):
+            if self._dbg:
                 log.info("BA[%s] = %s", i, ba[i])
             ba[i] = (ba[i] + offset - 65) % 63 + 65
-            if (self._dbg):
+            if self._dbg:
                 log.info("BA[%s] = %s", i, ba[i])
 
-        if (self._dbg):
+        if self._dbg:
             log.info("BA: %s", ba)
         return ba.decode(encoding)
 
-    def encrypt(self, input_str: str, encoding : str = "utf8") -> str:
+    def encrypt(self, input_str: str, encoding: str = "utf8") -> str:
         """Encrypt the string using a Caesar Cipher"""
         return self._modify(input_str, self._cipher_length, encoding)
-    
-    def decrypt(self, input_str: str, encoding : str = "utf8") -> str:
+
+    def decrypt(self, input_str: str, encoding: str = "utf8") -> str:
         """Decrypt the string using a Caesar Cipher"""
         return self._modify(input_str, -self._cipher_length, encoding)
--- easy_caesar/tests/test_caesar.py
+++ easy_caesar/tests/test_caesar.py
@@ -2,6 +2,7 @@
 #
 from easy_caesar import Cipher
 
+
 def test_encrypt():
     cipher = Cipher(1)
-    assert cipher.encrypt('aaaa') == 'bbbb'
+    assert cipher.encrypt("aaaa") == "bbbb"
